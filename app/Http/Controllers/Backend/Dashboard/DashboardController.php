5f046447c9bb6d9cc4b1d9defe042a41bb7bb0bf
prantokumar <officialprantosaha@gmail.com>
2023-09-02 18:27:29 +0600

    dashboard cards with count backend

--- app/Http/Controllers/Backend/Dashboard/DashboardController.php
+++ app/Http/Controllers/Backend/Dashboard/DashboardController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Backend\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Enum\ExamEnum;
+use App\Http\Controllers\Enum\UserTypeEnum;
+use App\Models\Exam;
 use App\Models\ExamQuestion;
 use App\Models\ExamSubmission;
 use App\Models\ExamSubmissionDetail;
 use App\Models\Question;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -17,7 +21,11 @@ class DashboardController extends Controller
     public function adminDashboard()
     {
         try {
-            return view('backend.pages.dashboard.dashboard');
+            $total_users = User::where('user_type', UserTypeEnum::USER)->get()->count();
+            $exams = Exam::where('status', ExamEnum::ACTIVE)->get()->count();
+            $questions = Question::where('status', 1)->get()->count();
+            $submissions = ExamSubmission::where('status', 1)->get()->count();
+            return view('backend.pages.dashboard.dashboard')->with(['total_users'=> $total_users, 'exams'=> $exams, 'questions' => $questions, 'submissions' => $submissions]);
         } catch (Exception $error) {
             Log::info('adminDashboard => Backend Error');
             Log::info($error->getMessage());
--- resources/views/backend/pages/dashboard/dashboard.blade.php
+++ resources/views/backend/pages/dashboard/dashboard.blade.php
@@ -5,13 +5,89 @@
 @stop
 
 @section('content')
-    <!--begin::Admin Dashbiard -->
-    <div class="container-fluid">
-        <!-- Page Heading -->
-        <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
-        <!-- Page Heading -->
+<!--begin::Admin Dashbiard -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+    <!-- Page Heading -->
+
+    <div class="row">
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Users</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total_users }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Exams</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $exams }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Questions
+                            </div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $questions }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Submissions</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $submissions }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <!--end::Admin Dashbiard -->
+
+</div>
+<!--end::Admin Dashbiard -->
 @stop
 
 @section('custom_scripts')
